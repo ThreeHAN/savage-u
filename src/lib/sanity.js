983947b3d34238356719bd2266b10b3446b4e85f
Andy Taylor <andy@mayberrywebcraft.com>
2026-02-15 12:39:05 -0600

    fix sanity

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
-  dataset: import.meta.env.VITE_SANITY_DATASET,
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID || 'waq8cp43',
+  dataset: import.meta.env.VITE_SANITY_DATASET || 'production',
   apiVersion: '2025-02-15',
   useCdn: false,
 });
